573624bca442d19621565ccd7ed9a9012ae400fd
tai160903 <ltt16092003@gmail.com>
2025-01-21 20:04:59 +0700

    add getDetail

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -6,11 +6,10 @@ const router = express.Router();
 router.post("/register", AuthController.register);
 router.post("/login", AuthController.login);
 router.post("/logout", AuthController.logout);
-router.post("/send-otp", AuthController.sendOtp);
-router.post("/verify-otp", AuthController.verifyOtp);
 router.post("/send-verify-email", AuthController.sendEmailVerify);
 router.get("/verify-email/:tokenVerify/:id", AuthController.verifyEmail);
 router.post("/reset-password", AuthController.sendEmailResetPassword);
+router.get("/user/:id", AuthController.getUserDetail);
 router.post(
   "/verify-reset-password/:token/:userId",
   AuthController.verifyEmailResetPassword
