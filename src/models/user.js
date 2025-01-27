f5689c202bdd1c66e0b9c1713aabde6d8fd8d69e
tai160903 <ltt16092003@gmail.com>
2025-01-27 15:46:08 +0700

    edit user model

--- src/models/user.js
+++ src/models/user.js
@@ -23,8 +23,8 @@ const userSchema = new mongoose.Schema(
     },
     role: {
       type: String,
-      enum: ["user", "admin"],
-      default: "user",
+      enum: ["customer", "staff", "manager", "admin"],
+      default: "customer",
     },
     tokenVerify: {
       type: String,
