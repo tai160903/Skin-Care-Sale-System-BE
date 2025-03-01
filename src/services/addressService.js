eb4120fa30ebb0f9dad02bb96b90f1e7ff720a57
tai160903 <ltt16092003@gmail.com>
2025-03-01 19:23:58 +0700

    edit

--- src/services/addressService.js
+++ src/services/addressService.js
@@ -1,20 +1,40 @@
 const AddressRepository = require("../repositories/addressRepository");
 
 const AddressService = {
-    getAllAddress: async () => {
-        return await AddressRepository.getAllAddress();
-    },
-    createAddress: async (address) => {
-        return await AddressRepository.createAddress(address); 
-    },
-    getWardsByDistrict: async (district) => {
-        return await AddressRepository.getWardsByDistrict(district);
-    },
-    getAllProvince: async () => {
-        return await AddressRepository.getAllProvince();
-    },        
-    getDistrictsByProvince: async (province) => {
-        return await AddressRepository.getDistrictsByProvince(province);
-    }
-}
+  getAllAddress: async () => {
+    const response = await AddressRepository.getAllAddress();
+    return {
+      message: "Get all address success",
+      data: response,
+    };
+  },
+  createAddress: async (address) => {
+    const response = await AddressRepository.createAddress(address);
+    return {
+      message: "Create address success",
+      data: response,
+    };
+  },
+  getWardsByDistrict: async (district) => {
+    const response = await AddressRepository.getWardsByDistrict(district);
+    return {
+      message: "Get wards by district success",
+      data: response,
+    };
+  },
+  getAllProvince: async () => {
+    const response = await AddressRepository.getAllProvince();
+    return {
+      message: "Get all province success",
+      data: response,
+    };
+  },
+  getDistrictsByProvince: async (province) => {
+    const response = await AddressRepository.getDistrictsByProvince(province);
+    return {
+      message: "Get districts by province success",
+      data: response,
+    };
+  },
+};
 module.exports = AddressService;
