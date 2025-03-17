d658bba4428221b59c1168b62d43b0cd5407e675
tai160903 <ltt16092003@gmail.com>
2025-03-17 19:25:32 +0700

    edit category

--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -1,19 +1,21 @@
 const CategoryRepository = require("../repositories/categoryRepository");
 
 const CategoryService = {
-    getAllCategories: async () => {
-        return await CategoryRepository.getAllCategories();
-    },
-    getCategoryById: async (id) => {
-        return await CategoryRepository.getCategoryById(id);
-    },    
-    createCategory: async (category) => {
-        return await CategoryRepository.createCategory(category);
-    },
-    updateCategory: async (id, category) => {     
-        return await CategoryRepository.updateCategory(id, category);
-    },
-    deleteCategory: async (id) => {
-        return await CategoryRepository.deleteCategory(id);    
-    },
-    }
+  getAllCategories: async () => {
+    return await CategoryRepository.getAllCategories();
+  },
+  getCategoryById: async (id) => {
+    return await CategoryRepository.getCategoryById(id);
+  },
+  createCategory: async (category) => {
+    return await CategoryRepository.createCategory(category);
+  },
+  updateCategory: async (id, category) => {
+    return await CategoryRepository.updateCategory(id, category);
+  },
+  deleteCategory: async (id) => {
+    return await CategoryRepository.deleteCategory(id);
+  },
+};
+
+module.exports = CategoryService;
