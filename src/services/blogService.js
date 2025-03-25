53483a8f3f6e95d7079bcd5528fa7cf68393fdbc
tai160903 <ltt16092003@gmail.com>
2025-03-26 01:14:27 +0700

    fix all validate

--- src/services/blogService.js
+++ src/services/blogService.js
@@ -2,8 +2,6 @@ const BlogRepository = require("../repositories/blogRepository");
 
 const BlogService = {
   async createBlog(blogdata) {
-    const { error } = createBlogSchema.validate(blogdata);
-    if (error) return { message: error.details[0].message, status: 400 };
     const blog = await BlogRepository.createBlog(blogdata);
     return {
       message: "Blog created successfully",
@@ -11,8 +9,6 @@ const BlogService = {
     };
   },
   async updateBlog(id, blogdata) {
-    const { error } = updateBlogSchema.validate(blogdata);
-    if (error) return { message: error.details[0].message, status: 400 };
     return await BlogRepository.updateBlog(id, blogdata);
   },
   async deleteBlog(id) {
--- src/services/categoryService.js
+++ src/services/categoryService.js
@@ -5,31 +5,15 @@ const CategoryService = {
     return await CategoryRepository.getAllCategories();
   },
   getCategoryById: async (id) => {
-    const { error } = getByIdCategorySchema.validate(id);
-    if (error) {
-      throw new Error(error.details[0].message);
-    }
     return await CategoryRepository.getCategoryById(id);
   },
   createCategory: async (category) => {
-    const { error } = createCategorySchema.validate(category);
-    if (error) {
-      throw new Error(error.details[0].message);
-    }
     return await CategoryRepository.createCategory(category);
   },
   updateCategory: async (id, category) => {
-    const { error } = updateCategorySchema.validate(category);
-    if (error) {
-      throw new Error(error.details[0].message);
-    }
     return await CategoryRepository.updateCategory(id, category);
   },
   deleteCategory: async (id) => {
-    const { error } = deleteCategorySchema.validate(id);
-    if (error) {
-      throw new Error(error.details[0].message);
-    }
     return await CategoryRepository.deleteCategory(id);
   },
 };
