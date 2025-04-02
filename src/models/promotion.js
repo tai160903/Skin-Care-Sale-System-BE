709b7c42d84116eeebb1eddd477ecb2e66ab2a6a
tai160903 <ltt16092003@gmail.com>
2025-04-02 15:39:08 +0700

    edit routine

--- src/models/promotion.js
+++ src/models/promotion.js
@@ -1,22 +1,23 @@
 const mongoose = require("mongoose");
 
-const PromotionSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  code: { type: String, required: true, unique: true },
-  description: { type: String },
-  customer_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: "Customer",
-            required: true,
-          },
-  image: { type: String },
-  discount_percentage: { type: Number, required: true, min: 0, max: 100 },
-  start_date: { type: Date, required: true },
-  end_date: { type: Date, required: true }
-}, {
-  timestamps: true
-});
-
+const PromotionSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    code: { type: String, required: true, unique: true },
+    description: { type: String },
+    customer_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Customer",
+    },
+    image: { type: String },
+    discount_percentage: { type: Number, required: true, min: 0, max: 100 },
+    start_date: { type: Date, required: true },
+    end_date: { type: Date, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 const Promotion = mongoose.model("Promotion", PromotionSchema);
 module.exports = Promotion;
